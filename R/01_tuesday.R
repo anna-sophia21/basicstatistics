94f162da6e2b3872e54ea4d30427773ad1feb235
anna-sophia21 <135127434+anna-sophia21@users.noreply.github.com>
2023-06-07 09:46:50 +0200

    update

--- R/01_tuesday.R
+++ R/01_tuesday.R
@@ -1,13 +1,32 @@
 library("usethis")
 library("gitcreds")
 
-use_git_config(user.name = "anna-sophia21", user.email = "anna-sophia.stocker@unibe.ch")
-gitcreds_get()
-gitcreds_set()
 
 use_git()
 usethis::use_github()
 
+library(tidyverse)
+library(lubridate)
+library(unibeCols)
 
-test = 20
-hhh
+install.packages("medicaldata")
+library(medicaldata)
+str(opt)
+head(opt)
+
+opt <- opt
+sup <- supraclavicular
+
+# data: opt
+data <- opt |>
+  select(PID, Clinic, Group, Age, Birthweight)
+
+ggplot(data, aes(x = Group, y = Birthweight))+
+  geom_boxplot()
+
+#data: sup
+supra <- supraclavicular |>
+  select(subject, group, gender, bmi, age, med_duration, opioid_total)
+
+ggplot(supra, aes(x = opioid_total, y = bmi, fill = group, colour = group))+
+  geom_point()
